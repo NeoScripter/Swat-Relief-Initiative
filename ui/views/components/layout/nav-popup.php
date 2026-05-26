a9579f5ca4b040f8154120bc85111fa56921b1bb
Neoscripter <sange0337@gmail.com>
2026-05-26 11:23:51 +0800

    Feat:add nav menu animation

--- ui/views/components/layout/nav-popup.php
+++ ui/views/components/layout/nav-popup.php
@@ -2,9 +2,9 @@
     id="nav-popup"
     class="backdrop:backdrop-blur-sm bg-white p-4 pb-8 max-w-100 w-full ml-auto md:hidden">
     <div>
-        <button 
-            aria-label="close modal" 
-            id="close-nav-menu-btn" 
+        <button
+            aria-label="close modal"
+            id="close-nav-menu-btn"
             class="text-5xl ml-auto font-normal size-8 grid place-content-center">
             &times;
         </button>
