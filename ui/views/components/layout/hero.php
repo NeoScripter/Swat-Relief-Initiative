2b9fc53fffe5989c57b7ef801348bdf7b6926275
Neoscripter <sange0337@gmail.com>
2026-05-26 15:20:06 +0800

    Fix:reduce h1's size on mobile screens

--- ui/views/components/layout/hero.php
+++ ui/views/components/layout/hero.php
@@ -1,3 +1,3 @@
 <section class="app-section bg-nav full-bleed px-4">
-    <h1 class="font-bold tracking-wider text-center text-5xl uppercase text-gray-50 md:text-6xl xl:text-7xl"><?= $heading ?></h1>
+    <h1 class="font-bold tracking-wider text-center text-4xl sm:text-5xl uppercase text-gray-50 md:text-6xl xl:text-7xl"><?= $heading ?></h1>
 </section>
