d61e5dc2e73cff24d25d4a1a4913dd1349f6e45a
Neoscripter <sange0337@gmail.com>
2026-05-26 18:33:34 +0800

    Style:make logo a link to the home page

--- ui/views/components/layout/header.php
+++ ui/views/components/layout/header.php
@@ -1,11 +1,15 @@
 <header class="bg-nav text-white px-(--px) xl:px-[calc(var(--px)/2)] sticky top-0 md:static z-10 pt-3 flex items-center gap-4 md:gap-6 lg:gap-8 xl:gap-10 justify-between">
-    <?= component('ui/adaptive-img', [
-        'sizes'    => 'mb',
-        'avif'     => true,
-        'path'     => 'shared/logo/logo',
-        'prtClass' => 'size-15 sm:size-25 xl:size-30 shrink-0',
-        'overlayClass' => 'rounded-full'
-    ]) ?>
+    <a href="/"
+        aria-label="Go to homepage"
+        class="block size-15 sm:size-25 xl:size-30 shrink-0">
+        <?= component('ui/adaptive-img', [
+            'sizes'    => 'mb',
+            'avif'     => true,
+            'path'     => 'shared/logo/logo',
+            'prtClass' => 'size-full',
+            'overlayClass' => 'rounded-full'
+        ]) ?>
+    </a>
     <?= component('layout/burger-menu', [
         'class'    => 'md:hidden mr-2 sm:size-12 sm:mr-4',
     ]) ?>
